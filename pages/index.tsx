3bf41db284103b8f6c18a67e01c13657b2c5f1ac
abida-parveen12 <abida.parveen@multiconnect.com.hk>
2024-09-11 18:35:52 +0500

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24 bg-gray-900 text-white">
+      <h1 className="text-3xl font-bold mb-6">Hello World</h1>
+    </main>
+  );
+}
